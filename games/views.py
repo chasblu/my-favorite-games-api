2f43027f7211e1117cbfbf1e1e2c126eea3fcae0
chasblu <c.prit@ymail.com>
2021-08-04 15:59:00 -0400

    fixed folders

--- games/views.py
+++ games/views.py
@@ -3,3 +3,19 @@ from rest_framework import generics
 from .models import
 # Create your views here.
 
+
+class GameList(generics.ListCreateAPIView):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+class GameDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = GameSerializer
+
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Artist.objects.all()
+    serializer_class = ReviewSerializer
