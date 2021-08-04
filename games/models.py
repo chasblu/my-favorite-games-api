867a12444816a90a9e506e48a9a670a2d9c3fce6
chasblu <c.prit@ymail.com>
2021-08-04 15:07:18 -0400

    Created Models

--- games/models.py
+++ games/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=255)
+    genre = models.CharField(max_length=255)
+    release_date = models.CharField(max_length=50)
+    preview_url = models.TextField()
+    rating = models.IntegerField()
+    owner = models.ForeignKey('users.User', related_name='games', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Review(models.Model):
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    game = models.ForeignKey(
+        Game, on_delete=models.CASCADE, related_name='reviews')
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey('users.User', related_name='reviews', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
